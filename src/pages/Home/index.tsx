542b2f6ac16eae5e4800697ae6554a722f91aa18
jefolidev <jefoliveira279@gmail.com>
2024-11-13 00:27:52 -0300

    countdown funcionando

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
 import { Play } from 'phosphor-react'
 
 import { zodResolver } from '@hookform/resolvers/zod'
+import { differenceInSeconds } from 'date-fns'
 import { useForm } from 'react-hook-form'
 import * as zod from 'zod'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import {
   CountDownContainer,
@@ -30,6 +31,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 
 export function Home() {
@@ -45,6 +47,20 @@ export function Home() {
     },
   })
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(
+        () =>
+          setAmountSecondsPassed(
+            differenceInSeconds(new Date(), activeCycle.startDate)
+          ),
+        1000
+      )
+    }
+  }, [activeCycle])
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
 
@@ -52,6 +68,7 @@ export function Home() {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     }
 
     setCycles((state) => [...state, newCycle])
@@ -59,8 +76,6 @@ export function Home() {
 
     reset()
   }
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
